6db25a82d86c2e230c659e0bf1424b54f48c2271
Oscar Tobias <osktobias@gmail.com>
2023-03-18 22:49:42 -0600

    Arreglo en eleccion de combustible

--- src/main/java/com/mycompany/proyectoprogramacion1/ProyectoProgramacion1.java
+++ src/main/java/com/mycompany/proyectoprogramacion1/ProyectoProgramacion1.java
@@ -20,165 +20,177 @@ public class ProyectoProgramacion1 {
         Carro carro = null;
         Balsa balsa = null;
         Avion avion = null;
-        do{
+        do {
             menuPrincipal();
             int opcion = scanner.nextInt();
             switch (opcion) {
-            case 1: //Objetos y Recursividad
-                menuVehiculo();
-                opcion = scanner.nextInt(); //Ingresar Opcion
+                case 1: //Objetos y Recursividad
+                    menuVehiculo();
+                    opcion = scanner.nextInt(); //Ingresar Opcion
 
-                switch (opcion) {
-                    case 1:
-                        tipoVehiculo();
-                        int tipo = scanner.nextInt();
-                        switch (tipo) {
-                            case 1: //Carro
-                                String comV;
-                                System.out.println("¿Que tipo de combustible usa el carro?:");
-                                System.out.println("1. Diesel");
-                                System.out.println("2. Gasolina");
-                                int opcom = scanner.nextInt();
-                                scanner.nextLine();
+                    switch (opcion) {
+                        case 1:
+                            tipoVehiculo();
+                            int tipo = scanner.nextInt();
+                            switch (tipo) {
+                                case 1: //Carro
+                                    int opcom = 0;
+                                    String comV = "";
+                                    while (opcom != 1 && opcom != 2) {
+                                        System.out.println("¿Qué tipo de combustible usa el carro?:");
+                                        System.out.println("1. Diesel");
+                                        System.out.println("2. Gasolina");
+                                        opcom = scanner.nextInt();
+                                        scanner.nextLine();
 
-                                if(opcom == 1){comV = "Diesel";}
-                                if(opcom == 2){comV = "Gasolina";}
-                                else{comV = "Invalido";}
+                                        switch (opcom) {
+                                            case 1:
+                                                comV = "Diesel";
+                                                break;
+                                            case 2:
+                                                comV = "Gasolina";
+                                                break;
+                                            default:
+                                                System.out.println("Por favor ingrese una opción válida (1 o 2)");
+                                                break;
+                                        }
+                                    }
 
-                                System.out.print("Ingrese la Marca del Vehiculo: ");
-                                String marcaV = scanner.nextLine();
+                                    System.out.print("Ingrese la Marca del Vehiculo: ");
+                                    String marcaV = scanner.nextLine();
 
-                                System.out.print("Ingrese el Ano de lanzamiento: ");
-                                int anoV = scanner.nextInt();
-                                scanner.nextLine();
+                                    System.out.print("Ingrese el Ano de lanzamiento: ");
+                                    int anoV = scanner.nextInt();
+                                    scanner.nextLine();
 
-                                System.out.print("Ingrese la Placa del Vehiculo: ");
-                                String placaV = scanner.nextLine();
+                                    System.out.print("Ingrese la Placa del Vehiculo: ");
+                                    String placaV = scanner.nextLine();
 
-                                System.out.print("Ingrese el nombre a registrar: ");
-                                String nombreV = scanner.nextLine();
+                                    System.out.print("Ingrese el nombre a registrar: ");
+                                    String nombreV = scanner.nextLine();
 
-                                carro = new Carro(marcaV, nombreV, anoV, placaV, comV);
-                                carro.mostrarInfo();
-                                carro.setTipoCombustible(comV);
-                                tipoV = 1;
-                                break;
+                                    carro = new Carro(marcaV, nombreV, anoV, placaV, comV);
+                                    carro.mostrarInfo();
+                                    carro.setTipoCombustible(comV);
+                                    tipoV = 1;
+                                    break;
 
-                            case 2: //Balsa
-                                String movBal;
-                                movimientoBalsa();
-                                int opBal = scanner.nextInt();
-                                scanner.nextLine();
+                                case 2: //Balsa
+                                    String movBal;
+                                    movimientoBalsa();
+                                    int opBal = scanner.nextInt();
+                                    scanner.nextLine();
 
-                                if(opBal == 1){movBal = "Motor";}
-                                if(opBal == 2){movBal = "Remo";}
-                                else{movBal = "Invalido";}
+                                    if (opBal == 1) {
+                                        movBal = "Motor";
+                                    }
+                                    if (opBal == 2) {
+                                        movBal = "Remo";
+                                    } else {
+                                        movBal = "Invalido";
+                                    }
 
-                                System.out.print("Ingrese la marca de la Balsa: ");
-                                String marcaB = scanner.nextLine();
+                                    System.out.print("Ingrese la marca de la Balsa: ");
+                                    String marcaB = scanner.nextLine();
 
-                                System.out.print("Ingrese el anio de lanzamiento: ");
-                                int anoB = scanner.nextInt();
-                                scanner.nextLine();
+                                    System.out.print("Ingrese el anio de lanzamiento: ");
+                                    int anoB = scanner.nextInt();
+                                    scanner.nextLine();
 
-                                System.out.print("Ingrese nombre a registrar: ");
-                                String nombreB = scanner.nextLine();
+                                    System.out.print("Ingrese nombre a registrar: ");
+                                    String nombreB = scanner.nextLine();
 
-                                balsa = new Balsa(marcaB, nombreB, anoB, movBal);
-                                balsa.setOpcion(movBal);
-                                balsa.mostrarInfo();
-                                tipoV = 2;
-                                break;
+                                    balsa = new Balsa(marcaB, nombreB, anoB, movBal);
+                                    balsa.setOpcion(movBal);
+                                    balsa.mostrarInfo();
+                                    tipoV = 2;
+                                    break;
 
-                            case 3: //Avion
-                                System.out.print("Ingrese la Cantidad de Pasajeros del Avion:");
-                                int pasajeros = scanner.nextInt();
-                                scanner.nextLine();
+                                case 3: //Avion
+                                    System.out.print("Ingrese la Cantidad de Pasajeros del Avion:");
+                                    int pasajeros = scanner.nextInt();
+                                    scanner.nextLine();
 
-                                System.out.print("Ingrese la marca del Avion: ");
-                                String marcaA = scanner.nextLine();
+                                    System.out.print("Ingrese la marca del Avion: ");
+                                    String marcaA = scanner.nextLine();
 
-                                System.out.print("Ingrese el anio de lanzamiento: ");
-                                int anoA = scanner.nextInt();
-                                scanner.nextLine();
+                                    System.out.print("Ingrese el anio de lanzamiento: ");
+                                    int anoA = scanner.nextInt();
+                                    scanner.nextLine();
 
-                                System.out.print("Ingrese nombre a registrar: ");
-                                String nombreA = scanner.nextLine();
+                                    System.out.print("Ingrese nombre a registrar: ");
+                                    String nombreA = scanner.nextLine();
 
-                                avion = new Avion(marcaA, nombreA, anoA, pasajeros);
-                                avion.mostrarInfo();
-                                tipoV = 3;
-                                break;
+                                    avion = new Avion(marcaA, nombreA, anoA, pasajeros);
+                                    avion.mostrarInfo();
+                                    tipoV = 3;
+                                    break;
 
                                 default:
                                     menu1 = true;
                                     break;
-                        }
-                        break;
-
-                    case 2: //Mostrar Datos Vehiculos
-                        if(tipoV == 1)
-                        {
-                            carro.mostrarInfo();
-                        }
-                        if(tipoV == 2)
-                        {
-                            balsa.mostrarInfo();
-                        }
-                        if(tipoV == 3)
-                        {
-                            avion.mostrarInfo();
-                        }
-                        if(tipoV != 1 && tipoV != 2 && tipoV != 3){
-                            System.out.println("No existen datos de vehiculos ingresados, por favor ingrese uno");
-                        }
-                        break;
-
-                    case 3: //Torres de Hanoi
-                                     
-                        Scanner sc = new Scanner(System.in);
-                        int n;
-                        
-                        do {
-                            System.out.print("Ingrese el número de discos (mínimo 3): ");
-                            n = sc.nextInt();
-                            if (n < 3) {
-                                System.out.println("Debe ingresar un mínimo de 3 discos para realizar la Torre de Hanoi.");
                             }
-                        } while (n < 3);
-                        
-                        int[][] torres = new int[3][n];
-                        TorreDeHanoi.llenarTorreInicial(torres);
-                        TorreDeHanoi.imprimirTorres(torres);
-                        TorreDeHanoi.resolverTorreDeHanoi(n, 0, 2, 1, torres);
-                        
-                        break;
+                            break;
 
-                    case 4: // Menu Principal
-                        menu1 = true;
-                        break;
-                }
-                break;
+                        case 2: //Mostrar Datos Vehiculos
+                            if (tipoV == 1) {
+                                carro.mostrarInfo();
+                            }
+                            if (tipoV == 2) {
+                                balsa.mostrarInfo();
+                            }
+                            if (tipoV == 3) {
+                                avion.mostrarInfo();
+                            }
+                            if (tipoV != 1 && tipoV != 2 && tipoV != 3) {
+                                System.out.println("No existen datos de vehiculos ingresados, por favor ingrese uno");
+                            }
+                            break;
 
-            case 2: //Fase 2
-                System.out.println("fase en contruccion");
-                break;
+                        case 3: //Torres de Hanoi
 
-            case 3: //Fase 3
-                System.out.println("Fase en construccion :D");
-                break;
+                            Scanner sc = new Scanner(System.in);
+                            int n;
 
-            case 4: //Salir
-                menu1 = false;
-                break;
+                            do {
+                                System.out.print("Ingrese el número de discos (mínimo 3): ");
+                                n = sc.nextInt();
+                                if (n < 3) {
+                                    System.out.println("Debe ingresar un mínimo de 3 discos para realizar la Torre de Hanoi.");
+                                }
+                            } while (n < 3);
 
-            default:
-                System.out.println("Se ha encontrado un error, vuelva a elegir una opcion");
-                break;
-        }
-        }while (menu1 == true);
+                            int[][] torres = new int[3][n];
+                            TorreDeHanoi.llenarTorreInicial(torres);
+                            TorreDeHanoi.imprimirTorres(torres);
+                            TorreDeHanoi.resolverTorreDeHanoi(n, 0, 2, 1, torres);
 
-        
+                            break;
+
+                        case 4: // Menu Principal
+                            menu1 = true;
+                            break;
+                    }
+                    break;
+
+                case 2: //Fase 2
+                    System.out.println("fase en contruccion");
+                    break;
+
+                case 3: //Fase 3
+                    System.out.println("Fase en construccion :D");
+                    break;
+
+                case 4: //Salir
+                    menu1 = false;
+                    break;
+
+                default:
+                    System.out.println("Se ha encontrado un error, vuelva a elegir una opcion");
+                    break;
+            }
+        } while (menu1 == true);
+
     }
 
     public static void menuPrincipal() {
