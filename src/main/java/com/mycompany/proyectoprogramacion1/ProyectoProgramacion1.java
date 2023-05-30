a8f48673809f05ac9e5ff64ca15ee70a87662e36
Oscar Tobias <osktobi>
2023-05-29 23:29:23 -0600

    Funcionalidad Aviones en bases de datos

--- src/main/java/com/mycompany/proyectoprogramacion1/ProyectoProgramacion1.java
+++ src/main/java/com/mycompany/proyectoprogramacion1/ProyectoProgramacion1.java
@@ -732,15 +732,15 @@ public class ProyectoProgramacion1 {
                                                 System.out.println("Desea Actualizar la información?: 1.)Si 2.)No");
                                                 int opUp = scanner.nextInt();
                                                 scanner.nextLine();
-                                                int opcomUp = 0;
+                                                int opbalUp = 0;
                                                 if (opUp == 1) {
-                                                    while (opcomUp != 1 && opcomUp != 2) {
+                                                    while (opbalUp != 1 && opbalUp != 2) {
                                                         System.out.println("¿Cómo se moverá la balsa?:");
                                                         System.out.println("1. Remo");
                                                         System.out.println("2. Motor");
-                                                        opcomUp = scanner.nextInt();
+                                                        opbalUp = scanner.nextInt();
                                                         scanner.nextLine();
-                                                        if (opcomUp == 1 || opcomUp == 2) {
+                                                        if (opbalUp == 1 || opbalUp == 2) {
 
                                                             System.out.print("Ingrese la Marca de la Balsa: ");
                                                             String marcaB = scanner.nextLine();
@@ -757,14 +757,14 @@ public class ProyectoProgramacion1 {
                                                             System.out.println("Marca: " + marcaB);
                                                             System.out.println("Nombre: " + nombreB);
                                                             System.out.println("Año: " + anoB);
-                                                            System.out.println("Movimiento Balsa: " + opcomUp);
+                                                            System.out.println("Movimiento Balsa: " + opbalUp);
                                                             System.out.println("");
                                                             System.out.print("Confirmar 1.)Si 2.)No: ");
                                                             int opconfirmar = scanner.nextInt();
 
                                                             switch (opconfirmar) {
                                                                 case 1:
-                                                                    balsaUp.setMovimientoBalsa(opcomUp);
+                                                                    balsaUp.setMovimientoBalsa(opbalUp);
                                                                     balsaUp.setMarca(marcaB);
                                                                     balsaUp.setAnio(anoB);
                                                                     balsaUp.setNombre(nombreB);
@@ -802,7 +802,8 @@ public class ProyectoProgramacion1 {
                                             }
                                         } catch (Exception e) {
                                             e.printStackTrace();
-                                        } /*finally {
+                                        }
+                                        /*finally {
                                             if (em != null && em.isOpen()) {
                                                 em.close();
                                             }
@@ -866,6 +867,231 @@ public class ProyectoProgramacion1 {
                                 System.out.println("5.) Volver al menu principal");
                                 System.out.print("Seleccione una opcion: ");
                                 opmen = scanner.nextInt();
+                                switch (opmen) {
+                                    case 1: //insertar aviones
+                                        Aviones avi = new Aviones();
+                                        System.out.println("Ingrese la cantidad de pasajeros en el Avion");
+                                        int pasajeros = scanner.nextInt();
+                                        scanner.nextInt();
+
+                                        System.out.print("Ingrese la marca del Avion: ");
+                                        String marcaA = scanner.nextLine();
+
+                                        System.out.print("Ingrese el Año de lanzamiento: ");
+                                        int anoA = scanner.nextInt();
+                                        scanner.nextLine();
+
+                                        System.out.print("Ingrese el nombre a registrar: ");
+                                        String nombreA = scanner.nextLine();
+
+                                        System.out.println("");
+                                        System.out.println("Resumen: ");
+                                        System.out.println("Marca: " + marcaA);
+                                        System.out.println("Nombre: " + nombreA);
+                                        System.out.println("Anio: " + anoA);
+                                        System.out.println("Numero de pasajeros: " + pasajeros);
+                                        System.out.println("");
+                                        System.out.print("Confirmar 1.)Si 2.)No: ");
+                                        int opconfirmar = scanner.nextInt();
+                                        switch (opconfirmar) {
+                                            case 1:
+                                                avi.setCantidadPasajeros(pasajeros);
+                                                avi.setMarca(marcaA);
+                                                avi.setAnio(anoA);
+                                                avi.setNombre(nombreA);
+                                                try {
+                                                    em.getTransaction().begin();
+                                                    em.persist(avi);
+                                                    em.getTransaction().commit();
+                                                    System.out.println("Id: " + avi.getAvionesID());
+                                                    System.out.println("Enviado a la base de datos :D");
+                                                } catch (Exception e) {
+                                                    em.getTransaction().rollback();
+                                                    e.printStackTrace();
+                                                    System.out.println("Ha ocurrido un error :(");
+                                                }
+                                                /*finally {
+                                                            em.close();
+                                                        }*/
+                                                break;
+
+                                            case 2:
+                                                System.out.println("Operacion Cancelada");
+                                                break;
+
+                                            default:
+                                                System.out.println("Operacion Cancelada");
+                                                break;
+                                        }
+                                        menu2 = false;
+                                        break;
+
+                                    case 2: //Leer Aviones
+                                        System.out.println("Seleccionar Aviones");
+                                        List<Aviones> lstAviones = new ArrayList<>();
+                                        AvionesJpaController aV = new AvionesJpaController(emf);
+                                        try {
+                                            lstAviones = aV.findAvionesEntities();
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                        /*finally {
+                                            em.close();
+                                        }*/
+
+                                        if (lstAviones.isEmpty() == false) {
+                                            for (Aviones av : lstAviones) {
+                                                System.out.println("Id: " + av.getAvionesID());
+                                                System.out.println("Marca: " + av.getMarca());
+                                                System.out.println("Nombre: " + av.getNombre());
+                                                System.out.println("Anio: " + av.getNombre());
+                                                System.out.println("Numero de pasajeros: " + av.getCantidadPasajeros());
+                                                System.out.println("---------------------------");
+                                            }
+                                        } else {
+                                            System.out.println("No hay datos para mostrar");
+                                        }
+                                        break;
+
+                                    case 3: //Actualizar Aviones
+                                        System.out.println("Actualizar Aviones");
+                                        System.out.print("Ingrese el ID del Avion: ");
+                                        int AvionID = scanner.nextInt();
+                                        scanner.nextLine();
+
+                                        AvionesJpaController av1 = new AvionesJpaController(emf);
+                                        Aviones avionUp = null;
+
+                                        try {
+                                            avionUp = av1.findAviones(AvionID);
+
+                                            if (avionUp != null) {
+                                                System.out.println("ID: " + avionUp.getAvionesID());
+                                                System.out.println("Marca: " + avionUp.getMarca());
+                                                System.out.println("Nombre: " + avionUp.getNombre());
+                                                System.out.println("Anio: " + avionUp.getAnio());
+                                                System.out.println("Numero de pasajeros: " + avionUp.getCantidadPasajeros());
+                                                System.out.println("------------------------------------");
+                                                System.out.print("¿Desea actualizar la Informacion? 1.)Si 2.)No: ");
+                                                int opUp = scanner.nextInt();
+                                                scanner.nextLine();
+                                                if (opUp == 1) {
+                                                    System.out.println("Ingrese la cantidad de pasajeros en el Avion");
+                                                    pasajeros = scanner.nextInt();
+                                                    scanner.nextInt();
+
+                                                    System.out.print("Ingrese la marca del Avion: ");
+                                                    marcaA = scanner.nextLine();
+
+                                                    System.out.print("Ingrese el Año de lanzamiento: ");
+                                                    anoA = scanner.nextInt();
+                                                    scanner.nextLine();
+
+                                                    System.out.print("Ingrese el nombre a registrar: ");
+                                                    nombreA = scanner.nextLine();
+
+                                                    System.out.println("");
+                                                    System.out.println("Resumen: ");
+                                                    System.out.println("Marca: " + marcaA);
+                                                    System.out.println("Nombre: " + nombreA);
+                                                    System.out.println("Anio: " + anoA);
+                                                    System.out.println("Numero de pasajeros: " + pasajeros);
+                                                    System.out.println("");
+                                                    System.out.print("Confirmar 1.)Si 2.)No: ");
+                                                    opconfirmar = scanner.nextInt();
+                                                    switch (opconfirmar) {
+                                                        case 1:
+                                                            avionUp.setCantidadPasajeros(pasajeros);
+                                                            avionUp.setMarca(marcaA);
+                                                            avionUp.setAnio(anoA);
+                                                            avionUp.setNombre(nombreA);
+                                                            try {
+                                                                em.getTransaction().begin();
+                                                                em.persist(avionUp);
+                                                                em.getTransaction().commit();
+                                                                System.out.println("Id: " + avionUp.getAvionesID());
+                                                                System.out.println("Enviado a la base de datos :D");
+                                                            } catch (Exception e) {
+                                                                em.getTransaction().rollback();
+                                                                e.printStackTrace();
+                                                                System.out.println("Ha ocurrido un error :(");
+                                                            }
+                                                            /*finally {
+                                                            em.close();
+                                                        }*/
+                                                            break;
+
+                                                        case 2:
+                                                            System.out.println("Operacion Cancelada");
+                                                            break;
+
+                                                        default:
+                                                            System.out.println("Operacion Cancelada");
+                                                            break;
+                                                    }
+                                                } else {
+                                                    System.out.println("Operacion Cancelada");
+                                                }
+
+                                            } else {
+                                                System.out.println("No se encontró ningun Avion con el AvionID especificado.");
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                        /*finally {
+                                            if (em != null && em.isOpen()) {
+                                                em.close();
+                                            }
+                                        }*/
+                                        break;
+
+                                    case 4: //Borrar Aviones
+                                        System.out.println("Borrar Aviones");
+                                        System.out.print("Ingrese el ID del Avion: ");
+                                        int avionIDDl = scanner.nextInt();
+                                        scanner.nextLine();
+
+                                        AvionesJpaController av2 = new AvionesJpaController(emf);
+                                        Aviones avionDl = null;
+
+                                        try {
+                                            avionDl = av2.findAviones(avionIDDl);
+
+                                            if (avionDl != null) {
+                                                System.out.println("Id: " + avionDl.getAvionesID());
+                                                System.out.println("Marca: " + avionDl.getMarca());
+                                                System.out.println("Nombre: " + avionDl.getNombre());
+                                                System.out.println("Año: " + avionDl.getAnio());
+                                                System.out.println("Numero de pasajeros: " + avionDl.getCantidadPasajeros());
+                                                System.out.println("---------------------------");
+                                                System.out.println("Desea Eliminar este Registro?: 1.)Si 2.)No");
+                                                int opDl = scanner.nextInt();
+                                                scanner.nextLine();
+                                                if (opDl == 1) {
+                                                    try {
+                                                        av2.destroy(avionIDDl);
+                                                        System.out.println("El Avion con ID " + avionIDDl + " ha sido borrado correctamente.");
+                                                    } catch (Exception e) {
+                                                        e.printStackTrace();
+                                                        System.out.println("Error al intentar borrar la balsa.");
+                                                    }
+                                                } else {
+                                                    System.out.println("Operación Cancelada.");
+                                                }
+
+                                            } else {
+                                                System.out.println("No se encontró ningun avion con la AvionID especificado.");
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    /*finally {
+                                            if (emf != null && emf.isOpen()) {
+                                                emf.close();
+                                            }
+                                        }*/
+                                }
                                 break;
 
                             case 4:
